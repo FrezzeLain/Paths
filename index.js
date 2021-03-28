7f003be37883724df7af808ec05adbb20c244aa2
FrezzeLain <luka-0703@mail.ru>
2021-03-28 21:47:09 +0300

    Авто - генерация и смена без условий

--- index.js
+++ index.js
@@ -1,62 +1,130 @@
-var StartMassiv = Array.from(document.querySelectorAll('.Image'));
-var RandomNumberMassiv = [];
-var IsClicked = false;
-var CurrentX, CurrentY, CurrentItem, CurrentId, CurrentTurn = 1;
+/*
+    Начальные переменные
+*/
+var MassivOfBlocks = [], RandomiseMassiv = [], MainBlock = document.getElementById('MainBlock'), BlockOfNumberValue = document.getElementById('NOB'), GenerateButton = document.getElementById('Generate'), RandomiseButton = document.getElementById('Randomise'), IsChoosed = false, CurrentId, CurrentBlock;
+/*
+    Прослушивание событий
+*/
+GenerateButton.addEventListener('click', function () {
+    var BlockValue = BlockOfNumberValue.value;
+    var Number = GetNumber(BlockValue);
+    var BlockWidthAndHeight = GenerateStyles(Number);
+    CreateBlocks(Number);
+    GenerateBlocks(Number, BlockWidthAndHeight, MassivOfBlocks);
+});
+RandomiseButton.addEventListener('click', function () {
+    RandomiseElements(MassivOfBlocks);
+});
+/*
+    Функции приложения
+*/
+function CreateBlocks(BlockAmount) {
+    for (var index = 1; index <= BlockAmount; index++) {
+        var Div = document.createElement('div');
+        Div.className = "Image absolute";
+        Div.id = "D" + index;
+        MassivOfBlocks.push(Div);
+    }
+}
+function GenerateStyles(Num) {
+    if (Num === 9)
+        return 220;
+    if (Num === 16)
+        return 165;
+    if (Num === 25)
+        return 132;
+}
+function RandomiseElements(ReadyArray) {
+    var ElementCount = ReadyArray.length;
+    var CountMassiv = [];
+    for (var index = 0; index < ElementCount; index++) {
+        var FLag = true;
+        while (FLag) {
+            var Random = RandomNumber(1, ElementCount);
+            if (!CountMassiv.includes(Random)) {
+                CountMassiv.push(Random);
+                FLag = false;
+            }
+        }
+    }
+    CountMassiv.forEach(function (ID) {
+        for (var i = 0; i < MassivOfBlocks.length; i++) {
+            if (MassivOfBlocks[i].id == "D" + ID) {
+                RandomiseMassiv.push(MassivOfBlocks[i]);
+                break;
+            }
+        }
+    });
+    var Number = RandomiseMassiv.length;
+    var Styles = GenerateStyles(Number);
+    MainBlock.innerHTML = '';
+    GenerateBlocks(Number, Styles, RandomiseMassiv);
+    RandomiseButton.style.display = 'none';
+    MainBlock.addEventListener('click', function (e) {
+        ClickBlock(e);
+    });
+}
+function ClickBlock(Event) {
+    var Block = Event.path[0];
+    var ArrayId = Block.id.split('');
+    ArrayId[0] = '';
+    var Id = Number(ArrayId.join(''));
+    if (!IsChoosed) {
+        CurrentId = Id;
+        CurrentBlock = Block;
+        IsChoosed = !IsChoosed;
+    }
+    else if (CurrentId == Id) {
+        console.log('...');
+    }
+    else {
+        ChangePlace(CurrentBlock, Block);
+        IsChoosed = !IsChoosed;
+    }
+}
+function ChangePlace(CurrBlock, NewBlock) {
+    var DopX = NewBlock.style.left;
+    var DopY = NewBlock.style.top;
+    NewBlock.style.left = CurrBlock.style.left;
+    NewBlock.style.top = CurrBlock.style.top;
+    CurrBlock.style.left = DopX;
+    CurrBlock.style.top = DopY;
+}
+function GenerateBlocks(Amount, WidthAndHeight, ReadyArray) {
+    var index = 0;
+    ReadyArray.forEach(function (Block) {
+        var BlockIdArray = Block.id.split('');
+        BlockIdArray[0] = '';
+        var BlockId = Number(BlockIdArray.join(''));
+        index += 1;
+        MainBlock.append(Block);
+        Block.style.width = WidthAndHeight + "px";
+        Block.style.height = WidthAndHeight + "px";
+        // Выяснить вертикальное положение элемента
+        var AmountInRow = Math.sqrt(Amount);
+        var CurrentRow = Math.floor((index - 1) / AmountInRow);
+        var CurrentY = CurrentRow * WidthAndHeight;
+        // Выяснить горизонтальное положение элемента
+        var CurrentColumn = (index - (CurrentRow * AmountInRow)) - 1;
+        var CurrentX = CurrentColumn * WidthAndHeight;
+        // Присвоить положение относительно родителя
+        Block.style.left = CurrentX + "px";
+        Block.style.top = CurrentY + "px";
+        var BackX = ((BlockId - (Math.floor((BlockId - 1) / AmountInRow) * AmountInRow)) - 1) * WidthAndHeight;
+        var BackY = Math.floor((BlockId - 1) / AmountInRow) * WidthAndHeight;
+        Block.style.backgroundPosition = "left -" + BackX + "px top -" + BackY + "px";
+    });
+}
+function GetNumber(Value) {
+    if (Value == 'default')
+        return 3 * 3;
+    if (Value == 'More')
+        return 4 * 4;
+    if (Value == 'More+')
+        return 5 * 5;
+}
 function RandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-StartMassiv.forEach(function (elem) {
-    var Flag = false;
-    while (!Flag) {
-        var Random = RandomNumber(1, 9);
-        if (!RandomNumberMassiv.includes(Random)) {
-            elem.id = "i" + CurrentTurn;
-            elem.classList.add("absolute" + Random);
-            RandomNumberMassiv.push(Random);
-            Flag = !Flag;
-            CurrentTurn += 1;
-        }
-    }
-});
-StartMassiv.forEach(function (elem) {
-    elem.addEventListener('click', function (e) {
-        Turn(e);
-    });
-});
-function Turn(e) {
-    if (!IsClicked) {
-        CurrentX = e.target.offsetLeft;
-        CurrentY = e.target.offsetTop;
-        CurrentItem = e.target;
-        CurrentId = Number(e.target.id.split('')[1]);
-        IsClicked = !IsClicked;
-    }
-    else {
-        var NewX = e.clientX;
-        var NewY = e.clientY;
-        var Div = document.elementFromPoint(NewX, NewY);
-        var ReadyX = Div.offsetLeft + 110;
-        var ReadyY = Div.offsetTop + 110;
-        var LeftAcceptedX = CurrentX - 110;
-        var LeftAcceptedY = CurrentY + 110;
-        var TopAcceptedX = CurrentX + 110;
-        var TopAcceptedY = CurrentY - 110;
-        var RigAcceptedX = CurrentX + 220 + 110;
-        var RigAcceptedY = CurrentY + 110;
-        var BottomAcceptedX = CurrentX + 110;
-        var BottomAcceptedY = CurrentY + 220 + 110;
-        if ((ReadyX == LeftAcceptedX) && (ReadyY == LeftAcceptedY) || (ReadyX == TopAcceptedX) && (ReadyY == TopAcceptedY) || (ReadyX == RigAcceptedX) && (ReadyY == RigAcceptedY) || (ReadyX == BottomAcceptedX) && (ReadyY == BottomAcceptedY)) {
-            CurrentItem.style.left = ReadyX - 110 + "px";
-            CurrentItem.style.top = ReadyY - 110 + "px";
-            Div.style.left = CurrentX + "px";
-            Div.style.top = CurrentY + "px";
-            var NewId = Number(Div.id.split('')[1]);
-            var RememberCurrentIndex = RandomNumberMassiv.indexOf(CurrentId);
-            var RememberNewIndex = RandomNumberMassiv.indexOf(NewId);
-            console.log("f1 = " + CurrentId + " F2 = " + NewId);
-            IsClicked = !IsClicked;
-        }
-    }
 }
--- index.ts
+++ index.ts
@@ -1,78 +1,169 @@
 
-    let StartMassiv:Array<HTMLDivElement> = Array.from(document.querySelectorAll('.Image'));
-    let RandomNumberMassiv:Array<number> = [];
-    let IsClicked: boolean = false;
-    let CurrentX: number, CurrentY: number, CurrentItem, CurrentId: number, CurrentTurn: number = 1;
+    /* 
+        Начальные переменные
+    */
+
+    let MassivOfBlocks: Array<HTMLDivElement> = []
+    , RandomiseMassiv: Array<HTMLDivElement> = []
+    , MainBlock: HTMLElement = document.getElementById('MainBlock')
+    , BlockOfNumberValue: HTMLSelectElement = document.getElementById('NOB')
+    , GenerateButton: HTMLElement = document.getElementById('Generate')
+    , RandomiseButton : HTMLElement = document.getElementById('Randomise')
+    , IsChoosed: boolean = false
+    , CurrentId: number
+    , CurrentBlock;
+
+    /*
+        Прослушивание событий
+    */
+
+    GenerateButton.addEventListener('click', () => {
+        const BlockValue = BlockOfNumberValue.value;
+        const Number = GetNumber(BlockValue);
+        const BlockWidthAndHeight = GenerateStyles(Number);
+
+        CreateBlocks(Number);
+        GenerateBlocks(Number, BlockWidthAndHeight, MassivOfBlocks);
+    });
+
+    RandomiseButton.addEventListener('click', () => {
+        RandomiseElements(MassivOfBlocks);
+    });
+
+    /*
+        Функции приложения
+    */
+
+    function CreateBlocks(BlockAmount: number):void {
+        for (let index = 1; index <= BlockAmount; index++) {
+            
+            const Div: HTMLDivElement = document.createElement('div');
+            Div.className = "Image absolute";
+            Div.id = `D${index}`;
+            MassivOfBlocks.push(Div);
+            
+        }
+    }
+
+    function GenerateStyles(Num: number): number {
+        if (Num === 9) return 220;
+        if (Num === 16) return 165;
+        if (Num === 25) return 132;
+    }
+
+    function RandomiseElements(ReadyArray: Array<HTMLDivElement>): void {
+
+        let ElementCount = ReadyArray.length;
+        let CountMassiv = [];
+
+        for (let index = 0; index < ElementCount; index++) {
+            let FLag = true;
+
+            while (FLag) {
+                let Random = RandomNumber(1, ElementCount);
+                
+                if(!CountMassiv.includes(Random)) {
+                    CountMassiv.push(Random);
+                    FLag = false;
+                }
+            }
+        }
+
+        CountMassiv.forEach((ID) => {
+
+            for (let i = 0; i < MassivOfBlocks.length; i++) {
+                if(MassivOfBlocks[i].id == `D${ID}`){
+                    RandomiseMassiv.push(MassivOfBlocks[i]);
+                    break;
+                }               
+            }
+        });
+
+        let Number = RandomiseMassiv.length;
+        let Styles = GenerateStyles(Number);
+
+        MainBlock.innerHTML = '';
+        GenerateBlocks(Number, Styles, RandomiseMassiv);
+        RandomiseButton.style.display = 'none';
+        MainBlock.addEventListener('click', (e) => {
+            ClickBlock(e);
+        })
+
+    }
+
+    function ClickBlock(Event): void {
+        let Block = Event.path[0];
+        let ArrayId = Block.id.split('');
+        ArrayId[0] = '';
+        let Id = Number(ArrayId.join(''));
+
+        if(!IsChoosed) {
+            CurrentId = Id;
+            CurrentBlock = Block;
+            IsChoosed = !IsChoosed;
+        } else if(CurrentId == Id) {
+            console.log('...');
+        } else{
+            ChangePlace(CurrentBlock, Block);
+            IsChoosed = !IsChoosed;
+        }
+    }
+
+    function ChangePlace(CurrBlock, NewBlock):void {
+        const DopX = NewBlock.style.left;
+        const DopY = NewBlock.style.top;
+
+        NewBlock.style.left = CurrBlock.style.left;
+        NewBlock.style.top = CurrBlock.style.top;
+
+        CurrBlock.style.left = DopX;
+        CurrBlock.style.top = DopY;
+
+    }
+
+    function GenerateBlocks(Amount: number, WidthAndHeight: number, ReadyArray: Array<HTMLDivElement>): void {
+        let index = 0;
+        ReadyArray.forEach((Block) => {
+            let BlockIdArray = Block.id.split('');
+            BlockIdArray[0] = '';
+            let BlockId = Number(BlockIdArray.join('')); 
+            index += 1;
+            MainBlock.append(Block);
+            Block.style.width = `${WidthAndHeight}px`;
+            Block.style.height = `${WidthAndHeight}px`;
+
+            // Выяснить вертикальное положение элемента
+
+            const AmountInRow = Math.sqrt(Amount);
+            const CurrentRow = Math.floor((index - 1) / AmountInRow);
+            const CurrentY = CurrentRow * WidthAndHeight;
+
+            // Выяснить горизонтальное положение элемента
+
+            const CurrentColumn = (index - (CurrentRow * AmountInRow)) - 1;
+            const CurrentX = CurrentColumn * WidthAndHeight;
+
+            // Присвоить положение относительно родителя
+
+            Block.style.left = `${CurrentX}px`;            
+            Block.style.top = `${CurrentY}px`;
+            
+            const BackX = ((BlockId - (Math.floor((BlockId - 1) / AmountInRow) * AmountInRow)) - 1) * WidthAndHeight;
+            const BackY = Math.floor((BlockId - 1) / AmountInRow) * WidthAndHeight;
+
+            Block.style.backgroundPosition = `left -${BackX}px top -${BackY}px`;
+
+        })
+    }
+
+    function GetNumber(Value: string):number {
+        if(Value == 'default') return 3 * 3; 
+        if(Value == 'More') return 4 * 4; 
+        if(Value == 'More+') return 5 * 5;
+    }
 
     function RandomNumber(min:number, max:number):number{
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    StartMassiv.forEach((elem) => {
-        let Flag = false;
-        while(!Flag){
-            let Random = RandomNumber(1, 9);
-            if(!RandomNumberMassiv.includes(Random)){
-                elem.id = `i${CurrentTurn}`;
-                elem.classList.add(`absolute${Random}`);
-                RandomNumberMassiv.push(Random);
-                Flag = !Flag;
-                CurrentTurn += 1;
-            }
-        }
-    })
-
-    StartMassiv.forEach((elem) => {
-        elem.addEventListener('click', (e:MouseEvent) => {
-            Turn(e);
-        })
-    });
-
-    function Turn(e:MouseEvent):void{
-        if(!IsClicked){
-            CurrentX = e.target.offsetLeft;
-            CurrentY = e.target.offsetTop;
-            CurrentItem = e.target;
-            CurrentId = Number(e.target.id.split('')[1]);
-            IsClicked = !IsClicked;
-        } else{
-            let NewX = e.clientX;
-            let NewY = e.clientY;
-            let Div:HTMLDivElement = document.elementFromPoint(NewX, NewY);
-
-            let ReadyX = Div.offsetLeft + 110;
-            let ReadyY = Div.offsetTop + 110;
-
-            let LeftAcceptedX = CurrentX - 110;
-            let LeftAcceptedY = CurrentY + 110;
-
-            let TopAcceptedX = CurrentX + 110;
-            let TopAcceptedY = CurrentY - 110;
-
-            let RigAcceptedX = CurrentX + 220 + 110;
-            let RigAcceptedY = CurrentY + 110;
-
-            let BottomAcceptedX = CurrentX + 110;
-            let BottomAcceptedY = CurrentY + 220 + 110;
-
-            if((ReadyX == LeftAcceptedX)&&(ReadyY == LeftAcceptedY) || (ReadyX == TopAcceptedX)&&(ReadyY == TopAcceptedY) || (ReadyX == RigAcceptedX)&&(ReadyY == RigAcceptedY) || (ReadyX == BottomAcceptedX)&&(ReadyY == BottomAcceptedY)){
-
-                CurrentItem.style.left = `${ReadyX - 110}px`;
-                CurrentItem.style.top = `${ReadyY - 110}px`;
-
-                Div.style.left = `${CurrentX}px`;
-                Div.style.top = `${CurrentY}px`;
-
-                let NewId = Number(Div.id.split('')[1]);
-
-                let RememberCurrentIndex = RandomNumberMassiv.indexOf(CurrentId);
-                let RememberNewIndex = RandomNumberMassiv.indexOf(NewId);
-
-                console.log(`f1 = ${CurrentId} F2 = ${NewId}`);
-
-                IsClicked = !IsClicked;
-            }
-        }
     }
